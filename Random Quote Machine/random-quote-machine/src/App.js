7696dc6c1afcaf033d3fb8a9388842ecd65e037c
wuyingge96@163.com <wuyingge96@163.com>
2020-05-19 01:43:53 +0800

    done basic things except css

--- Random Quote Machine/random-quote-machine/src/App.js
+++ Random Quote Machine/random-quote-machine/src/App.js
@@ -1,49 +1,91 @@
 import React from 'react';
 import './App.css';
-import Redux from "rudux";
+import {createStore} from "redux";
+import {Provider, connect} from "react-redux";
 
-const NumberOfProverb = 30;
-const changeRandom = (NumberOfProverb) => parseInt(Math.random()*NumberOfProverb);
+const INITIALNUMBER = Math.random();
+
+const VERB_AND_AUTHOR = "abcdefghijklmnopqrstuvwxyz".split("").map(letter => {
+  return {
+    author: letter,
+    text: ((letter) => {var str = ""; for (let i=0; i<30; i++) {str+=letter}; return str})(letter)
+  }
+});
+const NumberOfProverb = VERB_AND_AUTHOR.length;
+const INITIALSTATEGENERATOR = (random) => {
+  return {
+  seed: parseInt(random*NumberOfProverb),
+  color: parseInt(random*7)
+  }
+};
+const INITIALSTATE = INITIALSTATEGENERATOR(INITIALNUMBER);
+
 
 //Redux:
 const NEWQUOTE = "NEWQUOTE";
 
-const reduxReducer = (state={}, action) => {
+const reduxReducer = (state=INITIALSTATE, action) => {
   switch (action.type) {
     case NEWQUOTE:
-      return {seed: changeRandom(NumberOfProverb)};
+      return {seed: action.seed,
+      color: action.color};
     default: 
       return state;
   }
 }
 
-const requestANewQuote = () => {
+
+
+const requestANewQuote = (random) => {
+  
   return {
-    type: NEWQUOTE
+    type: NEWQUOTE,
+    seed: parseInt(random * VERB_AND_AUTHOR.length),
+    color: parseInt(random * 7)
   };
 }
 
-const store = Redux.createStore();
+const store = createStore(reduxReducer);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleClick: random => { 
+      return dispatch(requestANewQuote(random))
+    }
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    seed: state.seed,
+    color: state.color
+  };
+}
+
+
 
 //React
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      random: Math.random()
+    }
     this.handleClick = this.handleClick.bind(this);
   };
 
-  handleClick(event) {
-    return changeRandom();
+  handleClick() {
+    this.setState(
+      {random: Math.random()}
+    );
   }
-
 
   render() {
     return <div className="colored" id="scene">
       <div className="noncolor" id="quote-box">
         <p className="transparent" id="text">
-          测试
-          <p className="transparent" id="author">测试作者</p>
+          {VERB_AND_AUTHOR[this.props.seed]["text"]}
+          <p className="transparent" id="author">{VERB_AND_AUTHOR[this.props.seed]["author"]}</p>
         </p>
         <br/>
         <a className="transparent" href="https://twitter.com/intent/tweet" id="tweet-quote">
@@ -52,7 +94,8 @@ class Board extends React.Component {
         <button 
           className="transparent" 
           id="new-quote"
-          onClick={this.handleClick}
+          onClick={this.props.handleClick(this.state.random)}
+          onClick={this.handleClick} 
         >
           换一个
         </button>
@@ -61,10 +104,13 @@ class Board extends React.Component {
   }
 }
 
+const Container = connect(mapStateToProps, mapDispatchToProps)(Board);
+
+
 function App() {
-  return <div>
-    <Board />
-  </div>;
+  return <Provider store={store}>
+    <Container />
+  </Provider>;
 }
 
 export default App;
